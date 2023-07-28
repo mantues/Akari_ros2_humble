acf6994ab20fdade66c4145937645ec23dcf465a
YH <mantues@yahoo.co.jp>
2023-07-29 05:45:03 +0900

    add akari_launch package

---
+++ akari_launch/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+
+package_name = 'akari_launch'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('akari_launch', '*launch.[pxy][yma]*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='akariros',
+    maintainer_email='mantues@yahoo.co.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
