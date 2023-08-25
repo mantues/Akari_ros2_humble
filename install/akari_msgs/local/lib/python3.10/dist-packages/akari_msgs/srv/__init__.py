64386a6cd1f8112360bc4d98407d05f91a6149f4
YH <mantues@yahoo.co.jp>
2023-08-26 07:03:48 +0900

    merge servo server fixed code

--- install/akari_msgs/local/lib/python3.10/dist-packages/akari_msgs/srv/__init__.py
+++
@@ -1,9 +0,0 @@
-from akari_msgs.srv._set_allout import SetAllout  # noqa: F401
-from akari_msgs.srv._set_display_color import SetDisplayColor  # noqa: F401
-from akari_msgs.srv._set_display_image import SetDisplayImage  # noqa: F401
-from akari_msgs.srv._set_display_text import SetDisplayText  # noqa: F401
-from akari_msgs.srv._set_dout import SetDout  # noqa: F401
-from akari_msgs.srv._set_joint_bool import SetJointBool  # noqa: F401
-from akari_msgs.srv._set_joint_float import SetJointFloat  # noqa: F401
-from akari_msgs.srv._set_pwmout import SetPwmout  # noqa: F401
-from akari_msgs.srv._trigger import Trigger  # noqa: F401
