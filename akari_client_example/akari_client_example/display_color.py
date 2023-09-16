fc9326fa2b32ffa73ef64cc0d72f012e4091ca64
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2023-09-16 18:15:01 +0900

    Fix m5 server

--- akari_client_example/akari_client_example/display_color.py
+++ akari_client_example/akari_client_example/display_color.py
@@ -3,60 +3,62 @@
 
 import rclpy
 from rclpy.node import Node
-
-from akari_msgs.srv import SetDisplayColor
-
-from akari_client import AkariClient
+from akari_msgs.srv import SetDisplayColor, SetDisplayColorRgb
 from akari_client.color import Color
+import time
 
-import random
-
-color_pair = ['BLACK','NAVY','DARKGREEN','DARKCYAN','MAROON','PURPLE','OLIVE',
-                'LIGHTGREY','DARKGREY','BLUE','GREEN','CYAN','RED','MAGENTA',
-                'YELLOW','WHITE','ORANGE','GREENYELLOW','PINK','RESET', 'SELECT']
 
 class display_color_client(Node):
-
     def __init__(self):
-        super().__init__('display_color_client_node')
+        super().__init__("display_color_client_node")
         # create client
-        self.cli = self.create_client(SetDisplayColor, 'set_display_color')
-        while not self.cli.wait_for_service(timeout_sec = 1.0):
-            self.get_logger().info('service not available, waiting again...')
-
-        # create request
-        self.req = SetDisplayColor.Request()
-
-
-    def send_request(self):
-        choice = random.choice(color_pair)
-        self.req.color = choice
-        if choice == 'SELECT':
-            self.req.color_var = [random.randint(0, 255),
-                                    random.randint(0, 255),
-                                    random.randint(0, 255)]
-        else:
-            self.req.color_var = [0, 0, 0]
+        self.cli_color = self.create_client(SetDisplayColor, "set_display_color")
+        self.cli_color_rgb = self.create_client(
+            SetDisplayColorRgb, "set_display_color_rgb"
+        )
+        while not self.cli_color.wait_for_service(
+            timeout_sec=1.0
+        ) and self.cli_color_rgb.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("service not available, waiting again...")
 
 
-        self.get_logger().info('Change color : %s' % (self.req))
-
-        self.future = self.cli.call_async(self.req)
-        rclpy.spin_until_future_complete(self, self.future)
-        return self.future.result()
-
-
-def main(args = None):
-    rclpy.init(args = args)
-    # create client
+def main(args=None):
+    rclpy.init(args=args)
     client = display_color_client()
-    # send request
-    response = client.send_request()
-    client.get_logger().info('Result: : %s' %(str(response.result)))
-    client.destroy_node()
 
+    print("STEP1. Set display color to white")
+    req = SetDisplayColor.Request()
+    req.color = "WHITE"
+    client.future = client.cli_color.call_async(req)
+    rclpy.spin_until_future_complete(client, client.future)
+    client.get_logger().info(f"Result: : {client.future.result().result}")
+    print("")
+    time.sleep(2)
+
+    print("STEP1. Set display color to red")
+    req = SetDisplayColor.Request()
+    req.color = "RED"
+    client.future = client.cli_color.call_async(req)
+    rclpy.spin_until_future_complete(client, client.future)
+    client.get_logger().info(f"Result: : {client.future.result().result}")
+    print("")
+    time.sleep(2)
+
+    print("STEP1. Set display color to (0, 255, 0)")
+    req = SetDisplayColorRgb.Request()
+    req.r = 0
+    req.g = 255
+    req.b = 0
+    client.future = client.cli_color_rgb.call_async(req)
+    rclpy.spin_until_future_complete(client, client.future)
+    client.get_logger().info(f"Result: : {client.future.result().result}")
+    print("")
+    time.sleep(2)
+
+    client.get_logger().info("Finish!")
+    client.destroy_node()
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
