4ab4a39473ace176e5ff09f417a50260e5ca17d8
YH <mantues@yahoo.co.jp>
2023-09-24 06:30:30 +0900

    pysen format

--- m5_server/setup.py
+++ m5_server/setup.py
@@ -1,34 +1,37 @@
 import os
 from glob import glob
+
 from setuptools import setup
 
-package_name = 'm5_server'
+package_name = "m5_server"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     package_data={
-        '': [
-            'akari_msgs',
+        "": [
+            "akari_msgs",
         ]
     },
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='akariros',
-    maintainer_email='akariros@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="akariros",
+    maintainer_email="akariros@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'm5_server = m5_server.m5_server:main',
+        "console_scripts": [
+            "m5_server = m5_server.m5_server:main",
         ],
     },
 )
