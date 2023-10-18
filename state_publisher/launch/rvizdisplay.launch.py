0a29d4ad9ce35d363a5dcb9f832051d506360543
Yoshimasa Horiuchi <45273263+mantues@users.noreply.github.com>
2023-10-19 07:04:54 +0900

    add urdf
    urdf and rviz

---
+++ state_publisher/launch/rvizdisplay.launch.py
@@ -0,0 +1,31 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    package_dir = get_package_share_directory("state_publisher")
+    urdf = os.path.join(package_dir, "urdf", "akari.urdf")
+    rviz = os.path.join(package_dir, "rviz", "akari.rviz")
+
+    return LaunchDescription([
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            arguments=[urdf]),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            arguments=[urdf]),
+            
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            arguments=['-d' + rviz]),
+    ])
--- state_publisher/setup.py
+++ state_publisher/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import setup
 
 package_name = "state_publisher"
@@ -9,6 +12,18 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
+        (
+            os.path.join("share", package_name, "urdf"),
+            glob(os.path.join("urdf/*")),
+        ),
+        (
+            os.path.join("share", package_name, "rviz"),
+            glob(os.path.join("rviz/*")),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
