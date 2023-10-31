3bc7126323c02f098d2ddea424a7170887dfc550
Yoshimasa Horiuchi <45273263+mantues@users.noreply.github.com>
2023-11-01 06:27:00 +0900

    Update gazebo.launch.py

--- state_publisher/launch/gazebo.launch.py
+++ state_publisher/launch/gazebo.launch.py
@@ -1,46 +1,64 @@
 import os
-import xacro
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
-from launch.substitutions import LaunchConfiguration
-def generate_launch_descript
-ion():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    robot_name = 'state_publisher'
-    world_file_name = 'empty.world'
+from launch_ros.actions import Node
+import xacro
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-    world = os.path.join(get_package_share_directory(
-        robot_name), 'akari_gazebo_world', world_file_name)
+def generate_launch_description():
 
-    urdf = os.path.join(get_package_share_directory(
-        robot_name), 'urdf', 'akari.urdf')
-
-    xml = open(urdf, 'r').read()
-
-    xml = xml.replace('"', '\\"')
-
-    swpan_args = '{name: \"my_robot\", xml: \"' + xml + '\" }'
-
-    file_path = get_package_share_directory(robot_name)
-    #os.environ['GAZEBO_MODEL_PATH'] =  file_path + "/share/state_publisher/akari_gazebo_model"
-    os.environ['GAZEBO_MODEL_PATH'] = os.path.join(get_package_share_directory('state_publisher'), 'akari_gazebo_model')
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = 'state_publisher'
     
-    #os.environ['GAZEBO_PLUGIN_PATH'] = file_path + "/share/state_publisher/akari_gazebo_model"
+    file_subpath = os.path.join(pkg_name, "urdf", "akari.urdf")
+    
+    rviz = os.path.join(pkg_name, "rviz", "akari.rviz")
 
+    model_dir = get_package_prefix('state_publisher')
+    print(model_dir)
+    install_dir = os.path.join(get_package_share_directory('state_publisher'),'akari_gazebo_model')
+    os.environ['GAZEBO_MODEL_PATH'] = install_dir
+    print(install_dir)
+
+    # Configure the node
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        arguments=[file_subpath]
+    )
+
+    # Configure the node
+    node_robot_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        arguments=[file_subpath]
+    )
+
+
+    # Configure the node
+    node_rviz2 = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d' + rviz]
+    )
+    
+    # Gazebo launch
+    gazebo =         ExecuteProcess(
+            cmd=["gazebo", "--verbose", "-s", "libgazebo_ros_factory.so"],
+            )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-entity', 'akari_spawn', '-topic', '/robot_description'],
+                        output='screen')
+    
+    # Run the node
     return LaunchDescription([
-        ExecuteProcess(
-            cmd=['gazebo', '--verbose', world,
-                 '-s', 'libgazebo_ros_factory.so'],
-            output='screen'),
-
-        ExecuteProcess(
-            cmd=['ros2', 'param', 'set', '/gazebo',
-                 'use_sim_time', use_sim_time],
-            output='screen'),
-
-        ExecuteProcess(
-            cmd=['ros2', 'service', 'call', '/spawn_akari',
-                 'gazebo_msgs/SpawnEntity', swpan_args],
-            output='screen'),
+        node_robot_state_publisher,
+        node_robot_state_publisher_gui,
+        node_rviz2,
+        gazebo,
+        spawn_entity
     ])
