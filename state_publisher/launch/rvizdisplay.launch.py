f012d5d8d4919497460c3e5c775e7a7ae4ee49ca
Yoshimasa Horiuchi <45273263+mantues@users.noreply.github.com>
2023-12-15 00:00:05 +0900

    Update rvizdisplay.launch.py

--- state_publisher/launch/rvizdisplay.launch.py
+++ state_publisher/launch/rvizdisplay.launch.py
@@ -2,12 +2,18 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
 
     package_dir = get_package_share_directory("state_publisher")
     urdf = os.path.join(package_dir, "urdf", "akari.urdf")
     rviz = os.path.join(package_dir, "rviz", "akari.rviz")
+    
+    condition_argument = DeclareLaunchArgument("use_gui", default_value="false")
+    condition = LaunchConfiguration('use_gui')
 
     return LaunchDescription([
 
@@ -15,14 +21,24 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
+            output='screen',
             arguments=[urdf]),
 
         Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
             name='joint_state_publisher_gui',
-            arguments=[urdf]),
-            
+            output='screen',
+            arguments=[urdf],
+            condition=IfCondition(condition)), # ここでconditionがtrueのときに起動するように指定,
+        
+        Node(
+            package='state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
+            arguments=[urdf],
+            condition=UnlessCondition(condition)), # ここでconditionがfalseのときに起動するように指定,            
         Node(
             package="rviz2",
             executable="rviz2",
